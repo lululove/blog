dfa2fb973c7b6f43ccda6023aa8d523c79296986
Lulu <admin@lulu-love.cc>
2015-05-18 23:20:02 +0800

    add laravel style to modify css and js

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,19 +3,12 @@
 <head>
     <title>Lulu</title>
 
-    <link rel="stylesheet" href="styles/reset.css" type="text/css">
-    <link rel="stylesheet" href="styles/styles.css" type="text/css">
-
-    <!-- 新 Bootstrap 核心 CSS 文件 -->
-    <link rel="stylesheet" href="bootstrap-3.3.4/css/bootstrap.css">
-
-    <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
-    <script src="jquery-1.11.3/jquery-1.11.3.js"></script>
-
-    <!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
-    <script src="bootstrap-3.3.4/js/bootstrap.js"></script>
-
-    <script src="js/main.js"></script>
+    {{HTML::style('styles/reset.css')}}
+    {{HTML::style('styles/styles.css')}}
+    {{HTML::style('bootstrap-3.3.4/css/bootstrap.css')}}
+    {{HTML::script('jquery-1.11.3/jquery-1.11.3.js')}}
+    {{HTML::script('bootstrap-3.3.4/js/bootstrap.js')}}
+    {{HTML::script('js/main.js')}}
 
 </head>
 <body>
