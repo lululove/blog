1d21e609dc4b987fd0c45068461025c433484a38
Lulu <admin@lulu-love.cc>
2015-07-01 23:33:53 +0800

    Modify sipmile panition

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -16,7 +16,7 @@ class IndexController extends BaseController {
         //$articles = Article::all();
         $categories = Category::all();
 
-        $articles = Article::Paginate(2);
+        $articles = Article::simplePaginate(5);
         return View::make('index')->with(array('articles' => $articles, 'categories' => $categories));
     }
 
